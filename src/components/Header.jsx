c01dbf75c9eefdbfc42ab5c4421f2acec45aaca4
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-02 18:24:19 +0100

    Creado componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+import { CategoryMenu } from './CategoryMenu'
+import { UserMenu } from './UserMenu'
+export function Header() {
+    return (
+        <header className="header">
+            <CategoryMenu/>
+            <h1 className="header-title">HEAVENLY</h1>
+            <UserMenu />
+        </header>
+    )
+}
