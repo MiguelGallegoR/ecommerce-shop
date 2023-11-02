027753c336ea2960900f78cbc42422f497a60276
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-02 18:24:35 +0100

    Creado componente CategoryMenu

---
+++ src/components/CategoryMenu.jsx
@@ -0,0 +1,12 @@
+export function CategoryMenu() {
+    return (
+        <div className="category-menu-container">
+            <ul className="category-menu">
+                <li>Category 1</li>
+                <li>Category 2</li>
+                <li>Category 3</li>
+                <li>Category 3</li>
+            </ul>
+        </div>
+    )
+}
