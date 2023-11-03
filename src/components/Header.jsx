68382e8f63e54dc66cac4a1f043f0bf84087da4a
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-03 16:49:03 +0100

    añadido divider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
+import { Divider } from 'antd'
 import '../styles/Header.css'
 import { CategoryMenu } from './CategoryMenu'
 import { UserMenu } from './UserMenu'
 export function Header() {
     return (
-        <header className="header">
+        <>
+         <header className="header">
             <CategoryMenu/>
             <h1 className="header-title">HEAVENLY</h1>
             <UserMenu />
         </header>
+        <Divider />
+        </>
+       
     )
 }
