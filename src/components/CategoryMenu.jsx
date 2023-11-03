08e8dd2ba5a7fcd04969252759613839b33883a4
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-03 16:43:17 +0100

    añadidas categorias

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,11 +1,12 @@
+
+import { categories } from "../mock/categories.js";
 export function CategoryMenu() {
     return (
         <div className="category-menu-container">
             <ul className="category-menu">
-                <li>Category 1</li>
-                <li>Category 2</li>
-                <li>Category 3</li>
-                <li>Category 3</li>
+               {categories.map((category) => (
+                   <li key={category.id}>{category.name}</li>
+               ))}
             </ul>
         </div>
     )
