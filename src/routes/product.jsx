ed0f1e2257f50c545b0646e82548be0395319a3f
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-09 18:19:28 +0100

    creada ruta producto

---
+++ src/routes/product.jsx
@@ -0,0 +1,11 @@
+import {useLoaderData} from 'react-router-dom';
+
+export default function Product () {
+    
+    return (
+        <div>
+            <h1>Product</h1>
+        
+        </div>
+    )
+}
