aeb3dd24b4bd16b5634a7bae9c57e716b3253937
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-09 18:17:26 +0100

    añadido outlet y getallproducts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { Header } from './components/Header'
-import 'antd'
-import './App.css'
-import { CategoryBreadcrumb } from './components/Category-Breadcrumb'
+import { Header } from "./components/Header";
+import "antd";
+import "./App.css";
+import { CategoryBreadcrumb } from "./components/Category-Breadcrumb";
+import { Outlet } from "react-router-dom";
+import { getAllProducts } from "./services/allProducts";
+export async function loader() {
+  const products = await getAllProducts();
+  return {products}
+}
 function App() {
-
   return (
     <>
-    <Header/>
-    <CategoryBreadcrumb/>
+      <Header />
+      <CategoryBreadcrumb />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
