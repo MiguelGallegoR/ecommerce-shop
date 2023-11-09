bb85d86b20d4cdf77ab069ea89307c589419e232
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-09 18:17:10 +0100

    creado router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App, {loader as rootLoader} from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useParams
+} from "react-router-dom";
 import './index.css'
-
+import ErrorPage from './routes/errorPage.jsx';
+import ListOfProducts from './routes/ListOfProducts.jsx';
+import Product from './routes/product.jsx';
+import { Gender } from './routes/gender.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "products/",
+        element: <ListOfProducts />
+      },
+      {
+        path: "products/:productId",
+        element: <Product />
+      },
+      {
+        path: "gender/:gender",
+        element: <Gender/>
+      },
+    ]
+  },
+  
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
