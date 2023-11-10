8dcdc43b31f2d1004fb39658028b35e309d13988
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-10 14:05:01 +0100

    creado query provider en el router

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App, {loader as rootLoader} from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import {
   createBrowserRouter,
   RouterProvider,
-  useParams
+  useParams,
 } from "react-router-dom";
-import './index.css'
-import ErrorPage from './routes/errorPage.jsx';
-import ListOfProducts from './routes/ListOfProducts.jsx';
-import Product from './routes/product.jsx';
-import { Gender } from './routes/gender.jsx';
+import "./index.css";
+import ErrorPage from "./routes/errorPage.jsx";
+import ListOfProducts from "./routes/ListOfProducts.jsx";
+import Product from "./routes/product.jsx";
+import { Gender } from "./routes/gender.jsx";
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-    loader: rootLoader,
+
     children: [
       {
         path: "products/",
-        element: <ListOfProducts />
+        element: <ListOfProducts />,
       },
       {
         path: "products/:productId",
-        element: <Product />
+        element: <Product />,
       },
       {
         path: "gender/:gender",
-        element: <Gender/>
+        element: <Gender />,
       },
-    ]
+    ],
   },
-  
 ]);
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+//1. Envolvemos la app con el query client provider
+const queryClient = new QueryClient()
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
