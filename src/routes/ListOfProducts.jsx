fd0b53d157f6ce9eca53966a88230b50d7bb2ea5
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-10 14:05:38 +0100

    creado query para listar productos

--- src/routes/ListOfProducts.jsx
+++ src/routes/ListOfProducts.jsx
@@ -1,7 +1,54 @@
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import getAllProducts from "../services/allProducts.js";
+import { Card } from "antd";
+import "../styles/ListOfProducts.css";
+
+const { Meta } = Card;
 export default function ListOfProducts() {
-    return (
-        <>
-            <h1>List of Products</h1>
-        </>
-    );
+  //2. uso el query cliente
+  const queryClient = useQueryClient();
+
+  //3. hago mi query
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const products = await getAllProducts();
+      if (!products) {
+        return [];
+      }
+      return products;
+    },
+  });
+
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <>
+      <h1>List of Products</h1>
+      <ul className="list-of-products">
+        {data.map((product) => (
+          <li key={product._id}>
+            <Card
+              hoverable
+              className="product-card"
+              cover={
+                <img
+                  alt="example"
+                  src={`http://localhost:1234/images${product.photo}`}
+                />
+              }
+            >
+              <Meta title={product.name} description={`${product.price}€`} />
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
