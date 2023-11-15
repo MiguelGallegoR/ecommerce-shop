f9dfefb3f5add223ee8c0e7f19d5c60f9a9dec03
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-15 17:05:31 +0100

    añadido componente migas de pan

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Header } from "./components/Header";
 import "antd";
 import "./App.css";
-import { CategoryBreadcrumb } from "./components/Category-Breadcrumb";
 import { Outlet } from "react-router-dom";
-
+import CategoryBreadcrumb from "./components/Category-Breadcrumb";
 function App() {
   return (
     <>
