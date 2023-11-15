aeb38ef4e25a25ed91b1bea26573a26b3f3628e9
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-15 11:19:47 +0100

    añadidos estilos y container

--- src/routes/ListOfProducts.jsx
+++ src/routes/ListOfProducts.jsx
@@ -30,25 +30,28 @@ export default function ListOfProducts() {
 
   return (
     <>
-      <h1>List of Products</h1>
-      <ul className="list-of-products">
-        {data.map((product) => (
-          <li key={product._id}>
-            <Card
-              hoverable
-              className="product-card"
-              cover={
-                <img
-                  alt="example"
-                  src={`${import.meta.env.VITE_URL_PREFIX}${product.photo}`}
-                />
-              }
-            >
-              <Meta title={product.name} description={`${product.price}€`} />
-            </Card>
-          </li>
-        ))}
-      </ul>
+      <div className="list-of-products-container">
+        <h1>List of Products</h1>
+        <ul className="list-of-products">
+          {data.map((product) => (
+            <li key={product._id} className="product">
+              <Card
+                hoverable
+                className="product-card"
+                cover={
+                  <img
+                    alt="example"
+                    src={`${import.meta.env.VITE_URL_PREFIX}${product.photo}`}
+                    className="product-image"
+                  />
+                }
+              >
+                <Meta title={product.name} description={`${product.price}€`} />
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
