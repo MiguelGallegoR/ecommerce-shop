3e11f4fd9606eec8c91273f9903b4fd5a6693df2
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-16 16:15:26 +0100

    creado servicio para conseguir todas las categorias

---
+++ src/services/allCategorys.js
@@ -0,0 +1,7 @@
+export async function getAllCategorys() {
+    const url = "http://localhost:1234/products/categorys";
+    const response = await fetch(url);
+    const data = response.json();
+    console.log(data);
+    return data;
+}
