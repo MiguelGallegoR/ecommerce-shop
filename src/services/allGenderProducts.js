7eb81e5e4a3e3231c75950d081e584fba61b7856
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-20 16:44:21 +0100

    eliminados console logs

--- src/services/allGenderProducts.js
+++ src/services/allGenderProducts.js
@@ -1,10 +1,8 @@
 //Devuelve todos los productos y todos los grupos por genero introducido
 export async function getAllProductsByGender(gender) {
-    console.log(gender)
     const url = `http://localhost:1234/products?gender=${gender}`;
     const response = await fetch(url);
     const data = response.json();
-    console.log(data);
     return data;
   }
   
