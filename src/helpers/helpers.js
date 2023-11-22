412babfec6fb552d3504d044d2121bef94716f2f
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-22 12:56:57 +0100

    creado helper para poner primera letra en mayusculas

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,5 +1,12 @@
 // Función para obtener N elementos aleatorios de un array
 export function getRandomProducts(array, num) {
-    const shuffled = array.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, num);
+}
+
+export function capitalizeFirstLetter(str) {
+  if (typeof str !== "string" || str.length === 0) {
+    return str; // Return the original value if it's not a non-empty string
   }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
