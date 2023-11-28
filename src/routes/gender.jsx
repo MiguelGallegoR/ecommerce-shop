0b36eb0ff116cc2435749ef5ca4d4b5ea2adf779
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-28 16:56:40 +0100

    añadidos customs hooks para refactorizacion

--- src/routes/gender.jsx
+++ src/routes/gender.jsx
@@ -1,42 +1,58 @@
-import { useParams } from "react-router-dom";
 import { CategoryBreadcrumb } from "../components/Category-Breadcrumb";
 import { useQueryClient, useQuery } from "@tanstack/react-query";
 import { Card } from "antd";
 import { getRandomProducts } from "../helpers/helpers.js";
 import getAllProductsByGender from "../services/allGenderProducts.js";
+import { useCategorys } from "../hooks/useCategorys.jsx";
 const { Meta } = Card;
+import { useParams, Link } from "react-router-dom";
+import { useProducts } from "../hooks/useProducts.jsx";
 export function Gender() {
   const { gender } = useParams();
   const queryClient = useQueryClient();
 
-  //3. hago mi query
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["products", gender],
-    queryFn: async () => {
-      const productsWithGroups = await getAllProductsByGender(gender);
-      const {allProducts, groups} = productsWithGroups
-      if (!productsWithGroups) {
-        return [];
-      }
-      const randomProducts = getRandomProducts(allProducts, 10);
-      return randomProducts;
-    },
-  });
+  // //3. hago mi query
+  // const { isPending, isError, data, error } = useQuery({
+  //   queryKey: ["products", gender],
+  //   queryFn: async () => {
+  //     const productsWithGroups = await getAllProductsByGender(gender);
+  //     const { allProducts, groups } = productsWithGroups;
+  //     if (!productsWithGroups) {
+  //       return [];
+  //     }
+  //     const randomProducts = getRandomProducts(allProducts, 10);
+  //     return randomProducts;
+  //   },
+  // });
 
-  if (isPending) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
+  const { isCategorysPending, isCategorysError, categorys } = useCategorys();
+  const { isPending, isError, allProducts } = useProducts({ gender });
+  const randomProducts = getRandomProducts(allProducts, 10);
   return (
     <div>
-
+      <ul className="category-list-of-groups">
+        {categorys?.map((category) => (
+          <Card
+            size="small"
+            key={category}
+            hoverable={true}
+            className="group-card"
+          >
+            <Link
+              to={`/${gender}/${category}`}
+              className="category-list-of-groups-link"
+            >
+              <h3 className="category-list-of-groups-title">
+                {category.toUpperCase()}
+              </h3>
+            </Link>
+          </Card>
+        ))}
+      </ul>
       <h1 className="list-of-products-title">Some {gender} products</h1>
       <div className="list-of-products-container">
         <ul className="list-of-products">
-          {data.map((product) => (
+          {randomProducts?.map((product) => (
             <li key={product._id} className="product">
               <Card
                 hoverable
