f21f9007b46025f3fcc5cbbd662f16879f76f63e
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-11-28 16:55:05 +0100

    añadido ? para tratar con asincronia

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,7 @@
 // Función para obtener N elementos aleatorios de un array
 export function getRandomProducts(array, num) {
-  const shuffled = array.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, num);
+  const shuffled = array?.sort(() => 0.5 - Math.random());
+  return shuffled?.slice(0, num);
 }
 
 export function capitalizeFirstLetter(str) {
