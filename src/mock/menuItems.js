2710a4445fd99aa671d1ba54e5e4b159b8b2fa3e
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-08 17:38:16 +0100

    creado mock

---
+++ src/mock/menuItems.js
@@ -0,0 +1,24 @@
+export const menuItems = {
+    genders: [
+      {
+        womens: "Women",
+      },
+      {
+        mens: "Men",
+      },
+      {
+        kids: "Kids",
+      },
+    ],
+    categories: [
+      {
+        accessories: "Accessories",
+      },
+      {
+        clothes: "Clothes",
+      },
+      {
+        shoes: "Shoes",
+      },
+    ],
+};
