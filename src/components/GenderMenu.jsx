a41cc6b6844b2c0099619785451974b35d6e887e
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-08 17:40:38 +0100

    reducida logica del menu de enrutamiento para evitar numero de llamadas

--- src/components/GenderMenu.jsx
+++ src/components/GenderMenu.jsx
@@ -5,130 +5,43 @@ import { capitalizeFirstLetter } from "../helpers/helpers";
 import { Dropdown } from "antd";
 import { useCategorys } from "../hooks/useCategorys.jsx";
 import { useGroups } from "../hooks/useGroups.jsx";
+import { menuItems } from "../mock/menuItems.js";
 export function GenderMenu() {
-  const genders = {
-    womens: "Women",
-    mens: "Men",
-    kids: "Kids",
-  };
-
-  const queryClient = useQueryClient();
-
-  const { isCategorysPending, isCategorysError, categorys } = useCategorys();
-  const { groups } = useGroups({ genders });
-  //2. query para todos los grupos
-
-  // const groups = useQueries({
-  //   queries: Object.entries(genders).map(([key]) => {
-  //     return {
-  //       queryKey: ["groups", key],
-  //       queryFn: async () => {
-  //         try {
-  //           return await getAllProductsByGender(key);
-  //         } catch (error) {
-  //           console.error(`Error fetching data for gender ${key}:`, error);
-  //           throw error; // Lanzar la excepción para que sea manejada por useQueries
-  //         }
-  //       },
-  //     };
-  //   }),
-  // });
-  // Verifica si todas las consultas se han resuelto
-  const allQueriesResolved = groups?.every((query) => query.isSuccess);
-
-  if (!allQueriesResolved) {
-    return <p>Algunas consultas están pendientes o han fallado...</p>;
-  }
-
-  const groupsData = groups?.map((gender) => {
-    const genderData = gender?.data;
-    const { groupsByCategory } = genderData;
-    return groupsByCategory.map((g) => {
-      const { groups, category } = g;
-
-      return { groups, category };
-    });
-  });
-
-  groupsData?.[0].push("womens");
-  groupsData?.[1].push("mens");
-  groupsData?.[2].push("kids");
+  
   return (
     <div className="gender-menu-container">
       <ul className="gender-menu">
-        {allQueriesResolved &&
-          Object.entries(genders).map(([key, value]) => {
-            return (
-              <li key={key} className="gender-menu-item-li">
-                <Dropdown
-                  menu={{
-                    items: [
-                      {
-                        key: "1",
-                        label: (
-                          <div className="dropdown-menu-item-container">
-                            {categorys?.map((categoryItem) => {
-                              let category = categoryItem;
-                              const filteredResult = groupsData?.filter(
-                                (item) => item.includes(key)
-                              );
-                              return (
-                                <div
-                                  key={categoryItem}
-                                  className="dropdown-menu-item-container"
-                                >
-                                  <Link
-                                    className="dropdown-menu-category"
-                                    to={`${key}/${categoryItem}`}
-                                  >
-                                    {capitalizeFirstLetter(categoryItem)}
-                                  </Link>
-                                  <div
-                                    className="dropdown-menu-group-container"
-                                    key={`${key}/${categoryItem}`}
-                                  >
-                                    {filteredResult[0]
-                                      ?.filter(
-                                        (group) =>
-                                          group.category === categoryItem
-                                      )
-                                      .map((filteredGroup) => (
-                                        <div key={`${key}/${categoryItem}`}>
-                                          {filteredGroup?.groups.map(
-                                            (groupElement) => (
-                                              <div key={`${groupElement}`}>
-                                                <Link
-                                                  className="dropdown-menu-group"
-                                                  to={`${key}/${categoryItem}/${groupElement}`}
-                                                >
-                                                  {capitalizeFirstLetter(
-                                                    groupElement
-                                                  )}
-                                                </Link>
-                                              </div>
-                                            )
-                                          )}
-                                        </div>
-                                      ))}
-                                  </div>
-                                </div>
-                              );
-                            })}
-                          </div>
-                        ),
-                      },
-                    ],
-                  }}
-                  placement="bottom"
-                  className="dropdown-menu"
-                >
-                  <Link to={`/${key}`} className="gender-menu-item">
-                    {value.toLocaleUpperCase()}
-                  </Link>
-                </Dropdown>
-              </li>
-            );
-          })}
+        {menuItems.genders.map((g) => (
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: 1,
+                  label: (
+                    <div className="dropdown-menu-item-container">
+                      {menuItems.categories.map((c) => (
+                        <Link
+                          to={`/${Object.keys(
+                            g
+                          )[0].toLowerCase()}/${Object.values(
+                            c
+                          )[0].toLowerCase()}`}
+                          key={Object.values(c)[0]}
+                        >
+                          {Object.values(c)[0].toUpperCase()}
+                        </Link>
+                      ))}
+                    </div>
+                  ),
+                },
+              ],
+            }}
+          >
+            <li key={Object.keys(g)[0]} className="gender-menu-item-li">
+              <Link to={`/${Object.keys(g)[0].toLowerCase()}`}>{Object.values(g)[0].toUpperCase()}</Link>
+            </li>
+          </Dropdown>
+        ))}
         <span className="gender-menu-item-li">BRANDS</span>
       </ul>
     </div>
