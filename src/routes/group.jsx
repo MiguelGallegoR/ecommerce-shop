6f36a7ef575335f7904783e5d13e46a1d04dfab6
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-10 17:04:50 +0100

    creado estado filters y añadidos componentes Filters y FilteredListOfProducts

--- src/routes/group.jsx
+++ src/routes/group.jsx
@@ -1,51 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useGroupProducts } from "../hooks/useGroupProducts";
 import { Card } from "antd";
 const { Meta } = Card;
 import "../styles/ListOfProducts.css";
+import { Filters } from "../components/Filters";
+import FilteredListOfProducts from "../components/FilteredListOfProducts";
 
 export default function Group() {
   const queryClient = useQueryClient();
+  const [filters, setFilters] = useState({
+    active: false,
+    size: null,
+    price: null,
+    discount: null,
+    searchWasMade: false,
+    products: [],
+  });
+  const { gender, group } = useParams();
 
-  const { gender, category, group } = useParams();
-  //TODO:
-  //todos los  productos por grupo
-  //infiniti scroll
-  //Componente Filters para ordenar por precio y talla
-  const { isPending, isError, allGroupProducts } = useGroupProducts({ group });
   return (
     <div>
-      <h1>
-        {gender.toUpperCase()} {group.toUpperCase()}
-      </h1>
-      <div className="list-of-products-container">
-        <ul className="list-of-products">
-          {allGroupProducts?.map((product) => (
-            <li key={product._id} className="product">
-              <Link to={`/products/${product._id}`} className="product-link">
-                <Card
-                  hoverable
-                  className="product-card"
-                  cover={
-                    <img
-                      alt="example"
-                      src={`${import.meta.env.VITE_URL_PREFIX}${product.photo}`}
-                      className="product-image"
-                    />
-                  }
-                >
-                  <Meta
-                    title={product.name}
-                    description={`${product.price}€`}
-                  />
-                </Card>
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div>
+        <h1>
+          {gender.toUpperCase()} {group.toUpperCase()}
+        </h1>
+        <h2>{filters.products?.length}</h2>
       </div>
+
+      <Filters filters={filters} setFilters={setFilters} />
+
+      <FilteredListOfProducts filters={filters} />
+      
     </div>
   );
 }
