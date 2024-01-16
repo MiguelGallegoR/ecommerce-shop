8bb2ac43407c801d0de952a91215cde6c97578e7
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-16 17:20:42 +0100

    mejorada validaciones de filtros

--- src/services/filterProducts.js
+++ src/services/filterProducts.js
@@ -1,20 +1,25 @@
 export async function getFilterProducts( group, price, size, discount ) {
-  if(!price && !size && !discount){
-    return
-  }
- 
-  try{
-    const allSize = size.map(s => {
-      let param = `size=${s}`
-      return param
-    }).join('&')
-    const url = `http://localhost:1234/products/byGroup?group=${group}&price=${price}&${allSize}&discount=${discount}`;
+  try {
+    // Si price, size o discount son undefined, se asigna un valor por defecto
+    const priceParam = price !== undefined ? `price=${price}` : '';
+    
+    const sizeParam = size
+      ? size
+          .map((s) => `size=${s}`)
+          .join('&')
+      : '';
+
+    const discountParam = discount !== undefined ? `discount=${discount}` : '';
+
+    // Se construye la URL con los parámetros
+    const url = `http://localhost:1234/products/byGroup?group=${group}&${priceParam}&${sizeParam}&${discountParam}`;
+
     const response = await fetch(url);
-    const data = response.json();
+    const data = await response.json();
     return data;
-  }
-  catch(error){
-    console.log(error)
+  } catch (error) {
+    console.error(error);
+    throw error; // Puedes relanzar el error para que sea manejado en el lugar que llame a esta función
   }
 }
 
