c88e71e377c8aa40f40dfb4d0beb2676e27f265d
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-19 13:57:35 +0100

    añadida logica de paginación

--- src/hooks/useGroupProducts.jsx
+++ src/hooks/useGroupProducts.jsx
@@ -1,40 +1,49 @@
 import React, { useEffect, useRef } from "react";
 import getAllGroupProducts from "../services/allGroupProducts";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 export const useGroupProducts = ({ group, gender, setFilters }) => {
   //3. hago mi query
   const {
     isPending,
     isError,
     data: allGroupProducts,
-  } = useQuery({
+    refetch,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["groupProducts", group],
-    queryFn: async () => {
-      return await getAllGroupProducts(group);
+    queryFn: async ({pageParam}) => {
+      return await getAllGroupProducts({group: group, pageParam});
     },
-    
-   
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      return lastPage.nextCursor}
   });
 
-  const allGroupProductsByGender = allGroupProducts?.filter((product) => {
-    return product.gender.some((productGender) => productGender.toLowerCase() === gender);
-  });
+  console.log("allGroupProducts ", allGroupProducts);
+  // console.log("allGroupProducts ", allGroupProducts?.pages[0].products);
+  // const allGroupProductsByGender = allGroupProducts?.products?.filter(
+  //   (product) => {
+  //     return product.gender.some(
+  //       (productGender) => productGender.toLowerCase() === gender
+  //     );
+  //   }
+  // );
 
-  //setear filters con el resultado de la query. De forma que cuando se añadan filtros, filtramos a partir de estos resultados.
-  //creo una referencia para evitar que se renderize de forma infinita. Ya que la query es una promesa que pasa de  isPending a isError o data.
+  // //setear filters con el resultado de la query. De forma que cuando se añadan filtros, filtramos a partir de estos resultados.
+  // //creo una referencia para evitar que se renderize de forma infinita. Ya que la query es una promesa que pasa de  isPending a isError o data.
 
-  const allGroupProductsByGenderRef = useRef(allGroupProductsByGender);
-  
-  useEffect(() => {
-    allGroupProductsByGenderRef.current = allGroupProductsByGender;
-    setFilters((prev) => ({
-      ...prev,
-      products: allGroupProductsByGenderRef.current,
-    }));
-    console.log('valor de allGroupProductsByGender ', allGroupProductsByGender);
-  }, [allGroupProducts]);
+  // const allGroupProductsByGenderRef = useRef(allGroupProductsByGender);
 
-
+  // useEffect(() => {
+  //   allGroupProductsByGenderRef.current = allGroupProductsByGender;
+  //   setFilters((prev) => ({
+  //     ...prev,
+  //     products: allGroupProductsByGenderRef.current,
+  //   }));
+  //   console.log("valor de allGroupProductsByGender ", allGroupProductsByGender);
+  // }, [allGroupProducts]);
 
   if (isPending) {
     return <span>Loading...</span>;
@@ -44,5 +53,5 @@ export const useGroupProducts = ({ group, gender, setFilters }) => {
     return <span>Error: {error.message}</span>;
   }
 
-  return { isPending, isError, allGroupProductsByGender};
+  return { isPending, isError, fetchNextPage, hasNextPage, isFetchingNextPage};
 };
