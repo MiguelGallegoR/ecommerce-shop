46275752785ff953ff2a9d38f9c6ae6bd16593d2
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-19 13:58:01 +0100

    modificada funcion para paginacion

--- src/services/allGroupProducts.js
+++ src/services/allGroupProducts.js
@@ -1,8 +1,9 @@
-export async function getAllGroupProducts(group) {
-  const url = `http://localhost:1234/products/byGroup?group=${group}`;
+export async function getAllGroupProducts({group, pageParam} ) {
+  const url = `http://localhost:1234/products/byGroup?group=${group}&page=${pageParam}`;
   const response = await fetch(url);
-  const data = response.json();
-  return data;
+  const data =  await response.json();
+  debugger
+  const nextCursor = Number(data.page) + 1;
+  return { products: data.resultProducts, nextCursor: nextCursor }
 }
-
 export default getAllGroupProducts;
