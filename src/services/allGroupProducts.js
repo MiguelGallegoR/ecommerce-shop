ecf3773eee149ab16de23eb19c60ee5189ff69d8
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-22 10:02:00 +0100

    eliminado debugger

--- src/services/allGroupProducts.js
+++ src/services/allGroupProducts.js
@@ -2,7 +2,6 @@ export async function getAllGroupProducts({group, pageParam} ) {
   const url = `http://localhost:1234/products/byGroup?group=${group}&page=${pageParam}`;
   const response = await fetch(url);
   const data =  await response.json();
-  debugger
   const nextCursor = Number(data.page) + 1;
   return { products: data.resultProducts, nextCursor: nextCursor }
 }
