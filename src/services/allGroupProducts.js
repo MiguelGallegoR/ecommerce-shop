f3a18736a39707ae5621c0e1cfca305e27baa533
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2024-01-25 11:06:34 +0100

    modificado servicio para que reciba filtros y evitar el uso de dos querys diferentes

--- src/services/allGroupProducts.js
+++ src/services/allGroupProducts.js
@@ -1,5 +1,12 @@
-export async function getAllGroupProducts({group, pageParam} ) {
-  const url = `http://localhost:1234/products/byGroup?group=${group}&page=${pageParam}`;
+export async function getAllGroupProducts({group, pageParam, price, size, discount} ) {
+   // Si price, size o discount son undefined, se asigna un valor por defecto
+   const priceParam = price !== undefined  ? `price=${price}` : "";
+
+   const sizeParam = size ? size.map((s) => `size=${s}`).join("&") : "";
+
+   const discountParam = discount !== undefined ? `discount=${discount}` : "";
+
+  const url = `http://localhost:1234/products/byGroup?group=${group}&${priceParam}&${sizeParam}&${discountParam}&page=${pageParam}`;
   const response = await fetch(url);
   const data =  await response.json();
   const nextCursor = Number(data.page) + 1;
